0450478aebd260096d9cb00da4e9c23b802751b5
bainaryho <rkswl233@naver.com>
2023-07-28 17:47:38 +0900

    [수정]account 네임스페이스 제거

--- account/urls.py
+++ account/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from account import views
 
-app_name = 'account'
+from account import views
 
 urlpatterns = [
     # path('login/', views.user_login, name='login')
-    # as_view()를 사용한 클래스 기반뷰로 변경
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.dashboard, name='dashboard'),
+    # 비밀번호 변경
+    path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
 ]
