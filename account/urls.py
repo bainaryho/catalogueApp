7f1ca71f4dc4245ea7f2095e0fcd4ae8b0180f89
bainaryho <rkswl233@naver.com>
2023-07-28 17:32:16 +0900

    [수정]로그인,로그아웃 클래스뷰

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-
-from account import views
+from django.contrib.auth import views as auth_views
 
 app_name = 'account'
 
 urlpatterns = [
-    path('login/', views.user_login, name='login')
+    # path('login/', views.user_login, name='login')
+    # as_view()를 사용한 클래스 기반뷰로 변경
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
